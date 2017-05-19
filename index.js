fa3292c2c4f4f50db8f7a5356bdc5c19697f6dab
Regan Huff <regan.huff@gmail.com>
2017-05-18 17:45:19 -0700

    Initial display of hours minutes seconds

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+
+  var now = new Date();
+  var h = now.getHours();
+  var m = now.getMinutes();
+  var s = now.getSeconds();
+  //will need to add leading zeros for single digit times
+
+  $(document).ready(function() { // anonymous function
+      var time = $('<p>' + h + ":" + m + ":" + s + '</p>');
+      $('#clock').append(time);
+  });
+
+
+// use setInterval method to use findTime function to update each second
+
+// var intervalID;
+//
+// function updateTime() {
+//   intervalID = setInterval(findTime, 1000);
+// }
+
+
+//in html
+// <body onload="updateTime();">

9bb178d2ddd44244670e4bb734ca995797e9b629
Danielle Birbal <birbalds@gmail.com>
2017-05-19 14:58:14 -0700

    Made a digital clock

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+$(document).ready(function() {
+
+  setInterval(function(){
+    var date = new Date();
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+
+    var twoDigits = function(time) {
+      if (time < 10) {
+        display = "0" + time;
+      }else {
+        display = time;
+      }
+
+      return display
+  };
+    var time = twoDigits(hours) + ":" + twoDigits(minutes) + ":" + twoDigits(seconds);
+    $('#clock').text(time);
+  }, 1000);
+
+
+});

dc2ee3bca4e2974c47a2c2385b43ac03f9cdc24a
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-19 15:29:30 -0700

    Created new date object, parsed out hours and minutes to display

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+$(document).ready(function() {
+  var time = new Date(Date.now());
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  $("#clock").append('<p>' + hours + ':' + minutes + '</p>');
+});

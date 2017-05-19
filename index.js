600d2f82b9192ab2477605d2c6df569a7d9dab1f
Kayla <kubicke.kayla@gmail.com>
2017-05-19 15:27:30 -0700

    Seconds change on clock.
    Entire clock correctly changes time based on the seconds.

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
 
 console.log('Working script');
 
-  var today = new Date();
-  $('#clock').append(today);
+  // var today = new Date();
+  // $('#clock').append(today);
+
+  var start = new Date();
+  var seconds = start.getSeconds();
+
+  setInterval(function() {
+      $('#clock').text(new Date(), seconds);
+});
 });

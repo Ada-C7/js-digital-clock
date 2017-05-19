1afe1ef5735434414a2eefb2975292b3c227cb73
Haby Randall <sow.haby@gmail.com>
2017-05-19 15:11:00 -0700

    Working on index.js, on Clock constructor

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+$(document).ready(function() {
+  var Clock = function() {
+    var referenceDate = [
+      { date: new Date(), hours: new Date().getHours(), minutes: new Date().getMinutes(), seconds: new Date().getSeconds()}
+    ];
+
+
+    // new Date();
+    // var hours = referenceDate.getHours();
+    // var minutes = referenceDate.getMinutes();
+    // var seconds = referenceDate.getSeconds();
+
+
+
+    // var newDate = this.referenceDate.getTime();
+
+
+
+};
+var gs = new Clock();
+var clock = $('#clock');
+var show = $('<h1>' + console.log(gs) + '<h1>');
+clock.append(show);
+});
+
+
+
+
+
+// module.exports = Clock;

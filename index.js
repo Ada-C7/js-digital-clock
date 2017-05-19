c68d5c3fb5ae013b8992faf0071b355088d344c6
Regan Huff <regan.huff@gmail.com>
2017-05-18 18:23:11 -0700

    Time now updates every second

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 
+  var findTime = function() {
   var now = new Date();
   var h = now.getHours();
   var m = now.getMinutes();
   var s = now.getSeconds();
   //will need to add leading zeros for single digit times
+  var time = $('<p>' + h + ":" + m + ":" + s + '</p>');
+  $('#clock').empty();
+  $('#clock').append(time);
 
-  $(document).ready(function() { // anonymous function
-      var time = $('<p>' + h + ":" + m + ":" + s + '</p>');
-      $('#clock').append(time);
+};
+
+
+  $(document).ready(function() {
+
+    setInterval(findTime, 1000);
+
   });
-
-
-// use setInterval method to use findTime function to update each second
-
-// var intervalID;
-//
-// function updateTime() {
-//   intervalID = setInterval(findTime, 1000);
-// }
-
-
-//in html
-// <body onload="updateTime();">

adf61857736895e82fddba78cb35308155f546f6
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-19 14:46:11 -0700

    added constructor and prototype function, need to finish methods for H, M, S

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+
+$(document).ready(function() {
+
+
+var Date = function() {
+  if (i < 10)
+  i = "0" + i;
+  return i;
+};
+
+Date.prototype = {
+  time: function() {
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+  }
+};
+});

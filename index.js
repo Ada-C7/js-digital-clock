e21abeb2feb127b09d3c2176a3148caa7a8e5ce5
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-19 14:47:24 -0700

    clock is displaying and updating with setInterval

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+$("document").ready(function() {
+  var intervalID = window.setInterval(updateClock, 1000);
+
+
+  function updateClock() {
+    var now = new Date();
+    var hours = now.getHours() % 12;
+    var minutes = now.getMinutes();
+    var seconds = Math.round(now.getSeconds() * 1000)/1000;
+    // console.log(bigDate);
+    var clockLocation = $("#clock");
+    clockLocation.html("<p>" + hours + " : " + minutes + " : " + seconds + "</p>");
+  }
+
+
+
+});

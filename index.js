38ed7c5e8fd892b2b164428349a29d8224c37ac5
Allison Northrop <agnorthrop@gmail.com>
2017-05-19 15:17:11 -0700

    got the clock showing up

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+$(document).ready(function() {
+  var date = $('<h1>' + new Date() + '</h1>');
+  // 	var currentHours = date.getHours();
+  // 	var currentMinutes = date.getMinutes();
+  // 	var currentSeconds = date.getSeconds();
+  //   var num = 9;
+  //   // console.log(date, currentHours, currentMinutes, currentSeconds);
+  //
+  // $('.clock').append(date);
+  // $('h1').append(num);
+  $('#clock').append(date);
+
+  for (var i = 0; i < 5; i++) {
+  // Will execute 5 times
+  var listItem = $('<li>' + i + '</li>');
+  $('.dynamic-list').append(listItem);
+}
+
+
+});
+
+
+
+
+    // var listItem = $('<li>' + i + '</li>');
+
+// new Date(year, month[, date[, hours[, minutes[, seconds[, milliseconds]]]]]);

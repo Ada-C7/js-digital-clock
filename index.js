46b64e0b629d95a1566c4556ff882994252e91b6
Ting Wong <hellotingwong@gmail.com>
2017-05-19 15:07:49 -0700

    added base functions to calculate time

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+
+var intervalID = window.setInterval(myCallback, 1000);
+
+function myCallback() {
+  var unix_time = Date.now();
+
+  var dateTime = Date(unix_time);
+  dateTime = dateTime.replace(' GMT-0700 (PDT)','');
+  console.log(dateTime);
+}

8d103110c8c5a1ffa3f7abacda1cd1793261a93d
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-19 15:37:11 -0700

    have html on DOM from script

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
 $(document).ready(function() {
 
 
-var Date = function() {
-  if (i < 10)
-  i = "0" + i;
-  return i;
-};
 
-Date.prototype = {
-  time: function() {
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
-  }
-};
+
+
+var sup = document.getElementById("clock");
+
+sup.innerHTML = ("<p>sup</p>");
+
+
+  // if (i < 10)
+  // i = "0" + i;
+  // return i;
+
 });

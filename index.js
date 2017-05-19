8b8f8f3e47ed5961fae3559f4c8ae3bc63d49be4
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-19 15:23:06 -0700

    displaying time and changing every second

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 console.log("hello - you got this");
 $(document).ready(function(){
   // time is assigned to the function date that will reterive the current date
-  var current_time = window.setInterval(displayTime, 1000);
+  var intervalID = setInterval( function(){ displayTime() }, 1000 );
 
-    function displayTime() {
-      $('div').append(Date);
+  function displayTime() {
+    var clock = document.getElementById('clock');
+    var currentTime = new Date;
 
-    };
+    clock.innerHTML = currentTime;
 
+    // $('div').append( currentTime.getHours() + currentTime.getMinutes() + currentTime.getSeconds() );
+    // $('div').append(Date);
+  };
 
-  // $('div').append(time.getHours());
-  // $('div').append(time.getMinutes());
-  // $('div').append(time.getSeconds());
+  function clearTime() {
+    clearInterval(intervalID);
+  };
+
+  //
 
 });

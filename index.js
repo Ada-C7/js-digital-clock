4cb2c23ffc2fa3bb76bfe4607b0cc694c04a880e
Brenna <bcmdarroch@gmail.com>
2017-05-19 15:17:44 -0700

    finished a clock :))

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 
-// Use the Date Library in Javascript to retrieve the current date and time information.
-  var time = Date.now();
-
-// Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
-
 // Use the setInterval method to utilize the function you've created to update each second.
+  getTime();
 
-// add this to
-$('#div').append();
+  var intervalSecond = window.setInterval(getTime, 1000);
+
+  function getTime() {
+    // Use the Date Library in Javascript to retrieve the current date and time information.
+    var timeNow = new Date();
+
+    // Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
+    var hours = ('0' + timeNow.getHours()).slice(-2);
+    var minutes = ('0' + timeNow.getMinutes()).slice(-2);
+    var seconds = ('0' + timeNow.getSeconds()).slice(-2);
+    var timeDisplay = $("<p>" + hours + " : " + minutes + " : " + seconds + "</p>");
+
+    $('div').html(timeDisplay);
+  }
+
+
 
 });

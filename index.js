df3e2aff5bd3540a426764a27607eb28112ee3fd
Brenna <bcmdarroch@gmail.com>
2017-05-19 14:26:16 -0700

    pseudocoded

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+$(document).ready(function() {
+
+// Use the Date Library in Javascript to retrieve the current date and time information.
+  var time = Date.now();
+
+// Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
+
+// Use the setInterval method to utilize the function you've created to update each second.
+
+// add this to
+$('#div').append();
+
+});

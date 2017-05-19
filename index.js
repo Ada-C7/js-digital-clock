e4df35bbd996dd91dd12a0374cd55394738bb641
add2point71dots <alcland@gmail.com>
2017-05-19 14:41:18 -0700

    displays clock but doesn't update automatically

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+$(document).ready(function() {
+
+  var getTime = function() {
+    var currentDate = new Date();
+    var hours = currentDate.getHours() % 12;
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+
+    $("#clock").append(hours + " : " + minutes + " : " + seconds);
+  };
+  getTime();
+});

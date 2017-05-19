646d2caa8709f2206c8fa61dcdad78c056e0823e
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-19 14:49:16 -0700

    have method that will print out current date time everysecond - but not deleting old time message

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+console.log("hello - you got this");
+$(document).ready(function(){
+  // time is assigned to the function date that will reterive the current date
+  var current_time = window.setInterval(displayTime, 1000);
+
+    function displayTime() {
+      $('div').append(Date);
+
+    };
+
+
+  // $('div').append(time.getHours());
+  // $('div').append(time.getMinutes());
+  // $('div').append(time.getSeconds());
+
+});

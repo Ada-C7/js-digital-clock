a2a6b136a32378a8431b89a35975120d47755631
Kayla <kubicke.kayla@gmail.com>
2017-05-19 15:40:00 -0700

    Removed unnecessary code.

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ $(document).ready(function() {
 
 console.log('Working script');
 
-  // var today = new Date();
-  // $('#clock').append(today);
-
-  var start = new Date();
-  var seconds = start.getSeconds();
-
   setInterval(function() {
-      $('#clock').text(new Date(), seconds);
+      $('#clock').text(new Date());
 });
 });

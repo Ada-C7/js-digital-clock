913563a1e7c5658927e4fbdc15ea7218a56098fa
Andrea Valliere <amvalliere@gmail.com>
2017-05-19 15:08:35 -0700

    JS added to give current time in desired format

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+var unix_time = Date.now();
+
+var local = Date(unix_time);
+local = local.replace(' GMT-0700 (PDT)', '');
+console.log(local);
+
+
+// var intervalID = window.setInterval(myCallback, 500);
+//
+// function myCallback() {
+//   // Your code here
+// }

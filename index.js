110e51b240e0af32ebf2ed3e9dc15881f0c0c477
Kelly Souza <kellysouza99@gmail.com>
2017-05-19 15:18:47 -0700

    Working time

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
   console.log("This is a test");
 
+  var clock_div = $('#clock');
 
-var clock = $('#clock');
-var text = $('<p>this</p>');
-clock.append(text);
+  set_time = function(){
+    var currentTime = new Date().toTimeString();
+    var test = Date.now();
+    // var timeFormatted = timeInMs.to
+    clock_div.text(currentTime.split(' ')[0]);
+  };
+
+  setInterval(set_time, 1000);
+
+
+
 
 });

7aae97be8f4263e057a10fd063b50787105266ad
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-19 15:15:00 -0700

    added weekday, day, month, year

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ $("document").ready(function() {
 
 
   function updateClock() {
+    var weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var now = new Date();
     var hours = now.getHours() % 12;
     var minutes = now.getMinutes();
     var seconds = Math.round(now.getSeconds() * 1000)/1000;
-    // console.log(bigDate);
-    var clockLocation = $("#clock");
-    clockLocation.html("<p>" + hours + " : " + minutes + " : " + seconds + "</p>");
+    var weekday = now.getDay();
+    var month = now.getMonth();
+    var day = now.getDate();
+    var year = now.getFullYear();
+    var zones = [$("#pacific"), $("#mountain"), $("#central"), $("#eastern")];
+    // var pacific = $("#pacific");
+    for (var zone in zones) {
+      zones[zone].html(weekdays[weekday+1] + ", " + day + " " + months[month+1] + " " + year + ", " + " " + hours + ":" + minutes + ":" + seconds);
+    }
   }
 
 

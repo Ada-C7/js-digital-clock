bec78574e3a5757509d85d40b8e3032bb55c87ef
Kelly Souza <kellysouza99@gmail.com>
2017-05-20 16:47:48 -0700

    CLeans up whitespace and unnecessary lines

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
 
-  console.log("This is a test");
-
   var clock_div = $('#clock');
 
   set_time = function(){
     var currentTime = new Date().toTimeString();
     var test = Date.now();
-    // var timeFormatted = timeInMs.to
     clock_div.text(currentTime.split(' ')[0]);
   };
 
   setInterval(set_time, 1000);
-
-
-
 
 });

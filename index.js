3f9d3db7318eb9a3de64d12cc07573b807ae9ae3
amb54 <ambarklund@hotmail.com>
2017-05-19 19:53:13 -0700

    It works : ).

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+$(document).ready(function() {
+
+  // var intervalID = window.setInterval(ticking, 1000);
+  var tick = window.setInterval(function(){ticking()}, 1000);
+
+  var ticking = function() {
+    var clock = new Date();
+    var hours = ((clock.getHours() < 10) ? '0' : '') + clock.getHours()
+    var minutes = ((clock.getMinutes() < 10) ? '0' : '') + clock.getMinutes()
+    var seconds = ((clock.getSeconds() < 10) ? '0' : '') + clock.getSeconds()
+    var timeNow = $('<p>' + hours + ':' + minutes+ ':' + seconds+ '</p>');
+    $('#clock').html(timeNow);
+  }
+
+});

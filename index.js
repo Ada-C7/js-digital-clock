ec6eff89a4f016a29045741479886a21c90b0d6c
Kelly Souza <kellysouza99@gmail.com>
2017-05-20 16:46:29 -0700

    Adds continental us timezones

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
 
-  console.log("This is a test");
+  var clockDiv = $('#clock');
 
-  var clock_div = $('#clock');
 
-  set_time = function(){
-    var currentTime = new Date().toTimeString();
-    var test = Date.now();
-    // var timeFormatted = timeInMs.to
-    clock_div.text(currentTime.split(' ')[0]);
-  };
+  var hoursToMilliseconds = (60 * 60 * 1000);
+  var offsetMST = 1;
+  var offsetCST = 2;
+  var offsetEST = 3;
 
-  setInterval(set_time, 1000);
+
+
+  var setTime = function(){
+    var currentTime = new Date().toTimeString().split(' ')[0];
+    var mountainST = new Date ((new Date().getTime() + offsetMST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+    var centralST = new Date ((new Date().getTime() + offsetCST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+    var easternST = new Date ((new Date().getTime() + offsetEST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+
+    clockDiv.text(currentTime + ' PST\n' + mountainST + ' MST\n' + centralST + ' CST\n' + easternST +  ' EST\n');
+    };
+
+  setInterval(setTime, 1000);
+
+
 
 
 

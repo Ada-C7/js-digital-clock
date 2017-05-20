37ed8d8c204ef30d85f9562cc0d53ea7c53d5754
Theresa Manney <theresa.m.17@gmail.com>
2017-05-20 15:12:24 -0700

    Clock displays am and pm, deleted comments

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 // Your code here
 $(document).ready(function() {
-  // var display = function() {
+  // function that will get the time
   var getRealTime = function() {
+    // new date object
     var getDate = new Date();
+    // splitting date so it just shows the time
     var time = getDate.toTimeString().split(' ')[0];
-    // var currentTime = getDate.setInterval();
-    // var printTime = $("<p>" + getDate.getHours() + ":" + getDate.getMinutes() +":"+ getDate.getSeconds() + "</p>");
-    // var printTime = function() {
-    //   var hour = getDate.getHours();
-    //   var minute = getDate.getMinutes();
-    //   var second = getDate.getSeconds();
-    //
-    // };
-    $("#clock").text(time);
+    // display if it am or pm
+    var ampm = getDate.getHours() < 12 ? " AM" : " PM";
+    // display time so its not a 24 hour clock
+
+    // putting javascript into the html page using jQuery
+    $("#clock").text(time + ampm);
   };
-
-
-
-  // $("#clock").append(printTime);
-  // };
+  //loop that keeps the clock counting every second
   var interval = setInterval(getRealTime, 1000);
 });

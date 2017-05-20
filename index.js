9e7ffe1d67dd37a0fe27236d67079bfd8f58cef7
Olivia <ssecretsharer@gmail.com>
2017-05-20 13:27:23 -0700

    baseline submit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+$("document").ready(function() {
+
+setInterval(rightNow, 1000);
+
+  function rightNow() {
+    var currentTime = "";
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    currentTime += hour + ":" + minute + ":" + second;
+
+
+    $("#clock").text(currentTime);
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

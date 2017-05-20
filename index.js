989b44151f275656f73b68770dbf95beb4d2a9d6
Theresa Manney <theresa.m.17@gmail.com>
2017-05-20 15:47:17 -0700

    We now have a 12 hour clock

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ $(document).ready(function() {
     // new date object
     var getDate = new Date();
     // splitting date so it just shows the time
-    var time = getDate.toTimeString().split(' ')[0];
+    var time = getDate.toTimeString().split(' ')[0].substring(2, 8);
     // display if it am or pm
     var ampm = getDate.getHours() < 12 ? " AM" : " PM";
     // display time so its not a 24 hour clock
-
+    var hours = getDate.getHours() > 12 ? (getDate.getHours() - 12) : getDate.getHours();
+    // zero in front of hour if its less than 10
+    var zeroToHours = hours < 10 ? "0" : "";
+    
     // putting javascript into the html page using jQuery
-    $("#clock").text(time + ampm);
+    $("#clock").text(zeroToHours + hours + time + ampm);
   };
   //loop that keeps the clock counting every second
   var interval = setInterval(getRealTime, 1000);

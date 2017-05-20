9619922a0c55a73b2cc203425687bbdd7a522c94
Theresa Manney <theresa.m.17@gmail.com>
2017-05-20 15:03:26 -0700

    The clock is ticking !

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // Your code here
 $(document).ready(function() {
-  var getDate = Date.now();
+  // var display = function() {
+  var getRealTime = function() {
+    var getDate = new Date();
+    var time = getDate.toTimeString().split(' ')[0];
+    // var currentTime = getDate.setInterval();
+    // var printTime = $("<p>" + getDate.getHours() + ":" + getDate.getMinutes() +":"+ getDate.getSeconds() + "</p>");
+    // var printTime = function() {
+    //   var hour = getDate.getHours();
+    //   var minute = getDate.getMinutes();
+    //   var second = getDate.getSeconds();
+    //
+    // };
+    $("#clock").text(time);
+  };
 
-  var printTime = $("<p>" + getDate + "</p>");
-  $("#clock").append(printTime);
 
 
+  // $("#clock").append(printTime);
+  // };
+  var interval = setInterval(getRealTime, 1000);
 });

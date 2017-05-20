b928dcbe891ee808cdd4bcaf80e874d466251bd1
Haby Randall <sow.haby@gmail.com>
2017-05-19 20:49:15 -0700

    Working on the set interval method)

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 $(document).ready(function() {
-  var Clock = function() {
-    var referenceDate = [
-      { date: new Date(), hours: new Date().getHours(), minutes: new Date().getMinutes(), seconds: new Date().getSeconds()}
-    ];
+
+var getTime = function() {
+  var referenceDate = new Date();
+  var hours = referenceDate.getHours();
+  var minutes = referenceDate.getMinutes();
+  var seconds = referenceDate.getSeconds();
+  var time = undefined;
 
 
-    // new Date();
-    // var hours = referenceDate.getHours();
-    // var minutes = referenceDate.getMinutes();
-    // var seconds = referenceDate.getSeconds();
+  // var newDate = this.referenceDate.getTime();
+if (hours > 12) {
+   time = 'AM';
+} else if (hours < 12) {
+  time = 'PM';
+}
 
+  var clock = $('#clock');
+  var show = (hours + " " + minutes + " " + seconds + " " + time);
+  clock.append(show);
 
-
-    // var newDate = this.referenceDate.getTime();
-
-
-
-};
-var gs = new Clock();
-var clock = $('#clock');
-var show = $('<h1>' + console.log(gs) + '<h1>');
-clock.append(show);
+  var intervalID = scope.setInterval(getTime, 1000);
 });
 
 
 
+
+
+
+// get timer
+//time won't update
+
+// change append to text
 
 
 // module.exports = Clock;

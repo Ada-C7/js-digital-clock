0ef81b3223d1375651c289ffa84281b902332c8b
Spatterjaaay <aja.pursova@gmail.com>
2017-05-21 15:19:25 -0700

    added showTime function, addZerofunction and setInterval

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+$(document).ready(function() {
+
+  var addZero = function(digit) {
+    if (digit < 10) {
+      digit = "0" + digit;
+    }
+    return digit;
+  };
+
+  var showTime = function() {
+    var today = new Date(),
+    hours = today.getHours(),
+    minutes = addZero(today.getMinutes()),
+    seconds = addZero(today.getSeconds()),
+    ampm = hours > 11 ? ' PM' : ' AM',
+    americanHours;
+
+    if (hours === 0) {
+      americanHours = 12;
+    } else if (hours < 13) {
+      americanHours = hours;
+    } else {
+      americanHours = addZero(hours % 12);
+    }
+
+    $("#clock").html(americanHours + ":" + minutes + ":" + seconds + ampm);
+  };
+  
+  setInterval(showTime, 1000);
+});

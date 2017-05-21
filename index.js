b5b191126b92504000e3b7a8783954f5ff6c7936
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-21 11:20:26 -0700

    function to get the time, wrapper to set the interval.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+$(document).ready(function() {
+
+
+  var intervalID = window.setInterval(myCallback, 1000);
+
+    function myCallback() {
+      var dt = new Date();
+
+      var currentTime = {
+        currentHour: dt.getHours(),
+        currentMinute: dt.getMinutes(),
+        currentSecond: dt.getSeconds()
+      };
+
+
+      var time = currentTime.currentHour + ":" + currentTime.currentMinute + ":" + currentTime.currentSecond;
+
+      $("#clock").html(time);
+    }
+
+});

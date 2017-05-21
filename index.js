9735ef85ae4bb78f22e49fe28fee02c6687af13a
Kayla <kubicke.kayla@gmail.com>
2017-05-21 12:42:10 -0700

    Clock updates based on individual characteristics.
    Updated clock changes based on several individual characteristics.
    Every second the clock updates (clock updates based on 1 second intervals).

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-
 $(document).ready(function() {
 
-console.log('Working script');
+  var clock = function() {
 
-  setInterval(function() {
-      $('#clock').text(new Date());
-});
+    var time = new Date();
+    var month = time.getMonth();
+    var date = time.getDate();
+    var year = time.getYear();
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+
+    var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds;
+
+    $('#clock').html(newTime);
+  };
+  setInterval(clock);
 });

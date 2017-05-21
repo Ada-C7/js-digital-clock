9fb57665acca6ee6650d23043e838db0305a1cc1
Haby Randall <sow.haby@gmail.com>
2017-05-21 15:58:24 -0700

    Adding logic to show two digit seconds and minutes

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 $(document).ready(function() {
 
-var getTime = function() {
-  var referenceDate = new Date();
-  var hours = referenceDate.getHours();
-  var minutes = referenceDate.getMinutes();
-  var seconds = referenceDate.getSeconds();
-  var time = undefined;
+  var getTime = function() {
+    var referenceDate = new Date();
+    var hours = referenceDate.getHours();
+    var minutes = referenceDate.getMinutes();
+    var seconds = referenceDate.getSeconds();
+    var time = "AM";
 
 
-  // var newDate = this.referenceDate.getTime();
-if (hours > 12) {
-   time = 'AM';
-} else if (hours < 12) {
-  time = 'PM';
-}
+    // var newDate = this.referenceDate.getTime();
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
 
-  var clock = $('#clock');
-  var show = (hours + " " + minutes + " " + seconds + " " + time);
-  clock.append(show);
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
 
-  var intervalID = scope.setInterval(getTime, 1000);
+    if (hours > 12) {
+      hours = hours - 12;
+      time = 'PM';
+    }
+
+    if (hours === 0) {
+      hours = 12;
+    }
+
+    var clock = $('#clock');
+    var show = (hours + " " + minutes + " " + seconds + " " + time);
+    clock.append(show);
+
+    var intervalID = scope.setInterval(getTime, 1000);
+  };
 });
 
 

64869762f4126965cc80f369aa0a020360842a57
add2point71dots <alcland@gmail.com>
2017-05-21 10:29:33 -0700

    clock updates automatically and displays date

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
+  window.setInterval(function(){ setClock(); }, 1000);
 
-  var getTime = function() {
+  var setClock = function() {
     var currentDate = new Date();
-    var hours = currentDate.getHours() % 12;
-    var minutes = currentDate.getMinutes();
-    var seconds = currentDate.getSeconds();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var weekday = days[currentDate.getDay()];
+    var month = months[currentDate.getMonth()];
+    var day = currentDate.getDate();
+    var year = currentDate.getFullYear();
+    var hours = currentDate.getHours();
+    var minutes = twoDigitify(currentDate.getMinutes());
+    var seconds = twoDigitify(currentDate.getSeconds());
+    var meridiem = hours < 12 ? "AM" : "PM";
 
-    $("#clock").append(hours + " : " + minutes + " : " + seconds);
+    $("#clock").html("<p>" + weekday + ", " + month + " " + day + ", " + year + "</p>");
+    $("#clock").append("<p>" + (hours % 12) + ":" + minutes + ":" + seconds + " " + meridiem + "</p>");
   };
-  getTime();
+
+  var twoDigitify = function(digit) {
+    return ("0" + digit).slice(-2);
+  };
+
+  setClock();
 });

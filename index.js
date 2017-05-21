cad742a407a2ebe79ec9c99bb6ee58a17791121a
Kayla <kubicke.kayla@gmail.com>
2017-05-21 13:30:25 -0700

    12-hour clock displays full year.
    am/pm is displayed based on hour of day.
    Clock changed from 24-hour format to 12-hour format.
    Clock displays full year.

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ var clock = function() {
 var time = new Date();
 var month = time.getMonth();
 var date = time.getDate();
-var year = time.getYear();
+var year = time.getFullYear();
 
-var hours = ('0'  + time.getHours()).slice(-2);
+var hours = ((('0'  + time.getHours()).slice(-2) + 11) %12 + 1);
 var minutes = ('0'  + time.getMinutes()).slice(-2);
 var seconds = ('0' + time.getSeconds()).slice(-2);
+var suffix = (time.getHours() >= 12)? 'pm' : 'am';
 
 
-var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds;
+var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds + " " + suffix;
 
 return $('#clock').html(newTime);
 };

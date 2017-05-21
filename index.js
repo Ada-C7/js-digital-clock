ec0a539eaf32f3ef9cacde9a4850903f62fb83ab
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-05-21 13:26:35 -0700

    finally figured out what setInterval does

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+var myClock = new Date;
+
+var changeTime = function() {
+  var myClock = new Date;
+  // var hoursNow = myClock.getHours();
+  // var minutesNow = myClock.getMinutes();
+  // var secondsNow = myClock.getSeconds();
+  var currentTime = new Date(myClock.getTime()).toLocaleString()
+
+  // var currentTime = hoursNow + ":" + minutesNow + ":" + secondsNow;
+  $('#clock').html(currentTime);
+}
+
+$(document).ready(function() {
+  setInterval('changeTime()', 1000);
+});

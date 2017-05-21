1e218dc47b98463d5496586be934133e0ce46e47
Kayla <kubicke.kayla@gmail.com>
2017-05-21 14:50:11 -0700

    Fixed hours variable.
    Hours is properly converted to 12-hour format.
    Hours displays a leading zero if convertedHour only contains one number.

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ $(document).ready(function() {
 
 
 
-  var clock = function() {
+var clock = function() {
 
-    var time = new Date();
-    var month = time.getMonth();
-    var date = time.getDate();
-    var year = time.getFullYear();
+var time = new Date();
 
-    var hours = ((('0'  + time.getHours()).slice(-2) + 11) %12 + 1);
-    var minutes = ('0'  + time.getMinutes()).slice(-2);
-    var seconds = ('0' + time.getSeconds()).slice(-2);
-    var suffix = (time.getHours() >= 12)? 'pm' : 'am';
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var month = monthNames[time.getMonth()];
+var date = time.getDate();
+var year = time.getFullYear();
+
+var convertedHour = (time.getHours() % 12 + 1);
+var hours = ('0'  + convertedHour).slice(-2);
+var minutes = ('0'  + time.getMinutes()).slice(-2);
+var seconds = ('0' + time.getSeconds()).slice(-2);
+var suffix = (time.getHours() >= 12)? 'pm' : 'am';
 
 
-    var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds + " " + suffix;
+var newTime = month + " " + date + ", " +  year + " " +  hours + ":" + minutes + ":" + seconds + " " + suffix;
 
-    return $('#clock').html(newTime);
-  };
-  clock();
-  setInterval(clock, 1000);
+return $('#clock').html(newTime);
+};
+		clock();
+    setInterval(clock, 1000);
 
 });

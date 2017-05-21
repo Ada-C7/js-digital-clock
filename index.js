37598cd38a0ec1c05a58b954353bb10fd42a531a
Anacelia Gillette <acgillette426@gmail.com>
2017-05-20 19:50:44 -0700

    Whoops forgot about git there

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+
+$(document).ready(function() {
+
+
+
+  var setTime = function() {
+    date = new Date();
+    time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    $('#clock').text(time);
+  };
+
+  setInterval(setTime, 1000);
+
+});

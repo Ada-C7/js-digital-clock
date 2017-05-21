5ee8aa9f6145f214e2baa7551af43af58457a383
add2point71dots <alcland@gmail.com>
2017-05-21 16:28:27 -0700

    cleaned up some formatting

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-  var clocks = { seattle: -7, cheyenne: -6 };
+  var clocks = { maui: -10, seattle: -7, asheville: -4, tokyo: 9 };
   var cities = Object.keys(clocks);
+
+  var twoDigitify = function(digit) {
+    return ("0" + digit).slice(-2);
+  };
+
+  var capitalizeFirstLetter = function(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  var setStructure = function() {
+    for (i = 0; i < cities.length; i++) {
+      var section = $("<section></section>").addClass(cities[i]);
+      section.append("<h4>" + capitalizeFirstLetter(cities[i]) + " Time</h4>");
+      section.append("<section class=\"date-time\"></section>");
+      $("#clock").append(section);
+    }
+  };
 
   var calcDate = function(offset) {
     var currentDate = new Date();
@@ -8,7 +25,8 @@ $(document).ready(function() {
     return new Date(utc + 3600000 * offset);
   };
 
-  var getClock = function(currentDate) {
+  var getClock = function(city) {
+    var currentDate = calcDate(clocks[city]);
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var weekday = days[currentDate.getDay()];
@@ -32,23 +50,10 @@ $(document).ready(function() {
   var setClocks = function() {
     for (i = 0; i < cities.length; i++) {
       var section = $("." + cities[i] + " .date-time");
-      var dateTime = getClock(calcDate(clocks[cities[i]]));
+      var dateTime = getClock(cities[i]);
 
       section.html("<p>" + dateTime.date + "</p>");
       section.append("<p>" + dateTime.time + "</p>");
-    }
-  };
-
-  var twoDigitify = function(digit) {
-    return ("0" + digit).slice(-2);
-  };
-
-  var setStructure = function() {
-    for (i = 0; i < cities.length; i++) {
-      var section = $("<section></section>").addClass(cities[i]);
-      section.append("<h4>" + cities[i] + ":</h4>");
-      section.append("<section class=\"date-time\"></section>");
-      $("#clock").append(section);
     }
   };
 

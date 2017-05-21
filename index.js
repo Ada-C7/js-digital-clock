b51d8ef59426d525b1ba8faa4761cc611811f36e
Kayla <kubicke.kayla@gmail.com>
2017-05-21 14:05:50 -0700

    Clock function is called before setInterval to avoid lag.

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ $(document).ready(function() {
 
 
 
-//};
-var clock = function() {
+  var clock = function() {
 
-var time = new Date();
-var month = time.getMonth();
-var date = time.getDate();
-var year = time.getFullYear();
+    var time = new Date();
+    var month = time.getMonth();
+    var date = time.getDate();
+    var year = time.getFullYear();
 
-var hours = ((('0'  + time.getHours()).slice(-2) + 11) %12 + 1);
-var minutes = ('0'  + time.getMinutes()).slice(-2);
-var seconds = ('0' + time.getSeconds()).slice(-2);
-var suffix = (time.getHours() >= 12)? 'pm' : 'am';
+    var hours = ((('0'  + time.getHours()).slice(-2) + 11) %12 + 1);
+    var minutes = ('0'  + time.getMinutes()).slice(-2);
+    var seconds = ('0' + time.getSeconds()).slice(-2);
+    var suffix = (time.getHours() >= 12)? 'pm' : 'am';
 
 
-var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds + " " + suffix;
+    var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds + " " + suffix;
 
-return $('#clock').html(newTime);
-};
-
-    setInterval(clock, 1000);
+    return $('#clock').html(newTime);
+  };
+  clock();
+  setInterval(clock, 1000);
 
 });

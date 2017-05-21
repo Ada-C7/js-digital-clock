dbda4c0035f8bb553acc0e8e222d41d13b2f9b9e
Kayla <kubicke.kayla@gmail.com>
2017-05-21 13:15:17 -0700

    Leading zeros are displayed for hour, minute, and second.

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
 
-  var clock = function() {
-
-    var time = new Date();
-    var month = time.getMonth();
-    var date = time.getDate();
-    var year = time.getYear();
-
-    var hours = time.getHours();
-    var minutes = time.getMinutes();
-    var seconds = time.getSeconds();
 
 
-    var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds;
+//};
+var clock = function() {
 
-    $('#clock').html(newTime);
-  };
-  setInterval(clock);
+var time = new Date();
+var month = time.getMonth();
+var date = time.getDate();
+var year = time.getYear();
+
+var hours = ('0'  + time.getHours()).slice(-2);
+var minutes = ('0'  + time.getMinutes()).slice(-2);
+var seconds = ('0' + time.getSeconds()).slice(-2);
+
+
+var newTime = month + "/" + date + "/" +  year + " " + hours + ":" + minutes + ":" + seconds;
+
+return $('#clock').html(newTime);
+};
+
+    setInterval(clock, 1000);
+
 });

dbb6751a3e8835d4f71dd00a653b45d262ca51da
add2point71dots <alcland@gmail.com>
2017-05-21 12:58:44 -0700

    fixed bug that was displaying times at 12 something as 0 something

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
   window.setInterval(function(){ setClocks(); }, 1000);
 
-  var getClock = function() {
+  var calcDate = function(offset) {
     var currentDate = new Date();
+    var utc = currentDate.getTime() + (currentDate.getTimezoneOffset() * 60000);
+    return new Date(utc + 3600000 * offset);
+  };
+
+  var getClock = function(currentDate) {
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var weekday = days[currentDate.getDay()];
     var month = months[currentDate.getMonth()];
     var day = currentDate.getDate();
     var year = currentDate.getFullYear();
-    var hours = currentDate.getHours();
+    var militaryHours = currentDate.getHours();
+    var hours = (militaryHours % 12 === 0) ? 12 : (militaryHours % 12);
     var minutes = twoDigitify(currentDate.getMinutes());
     var seconds = twoDigitify(currentDate.getSeconds());
-    var meridiem = hours < 12 ? "AM" : "PM";
+    var meridiem = militaryHours < 12 ? "AM" : "PM";
 
     var dateTime = {
-      date: "<p>" + weekday + ", " + month + " " + day + ", " + year + "</p>",
-      time: "<p>" + (hours % 12) + ":" + minutes + ":" + seconds + " " + meridiem + "</p>"
+      date: weekday + ", " + month + " " + day + ", " + year,
+      time: hours + ":" + minutes + ":" + seconds + " " + meridiem
     };
 
     return dateTime;
   };
 
   var setClocks = function() {
-    var dateTime = getClock();
-    $("#clock").html(dateTime.date);
-    $("#clock").append(dateTime.time);
+    var clocks = { Seattle: -7 };
+    var cities = Object.keys(clocks);
+    for (i = 0; i < cities.length; i++) {
+      var dateTime = getClock(calcDate(clocks[cities[i]]));
+      $("#clock").html("<p>" + dateTime.date + "</p>");
+      $("#clock").append("<p>" + dateTime.time + "</p>");
+    }
   };
 
   var twoDigitify = function(digit) {

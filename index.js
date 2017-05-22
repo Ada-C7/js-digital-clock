82681209a82e6377ce67c1c289a329a54ba54e9b
Janice Lichtman <jalichtman@gmail.com>
2017-05-22 08:02:32 -0700

    have EST working

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 $(document).ready(function() {
 
 
-
-
-
-
-  var clockDiv = $('#clock');
-
-  var offsetMST = 1;
-  var offsetCST = 2;
-  var offsetEST = 3;
-
-  //  var timer= function(){
-  //    var currentTime = new Date().toTimeString().split(' ')[0];
-  //    var mountainST = new Date ((new Date().getTime() + offsetMST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-  //    var centralST = new Date ((new Date().getTime() + offsetCST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-  //    var easternST = new Date ((new Date().getTime() + offsetEST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-  //
-  //     clockDiv.text(currentTime + ' PST\n' + mountainST + ' MST\n' + centralST + ' CST\n' + easternST +  ' EST\n');
-  //     };
-
-
-
-
   var timer = function() {
     var today = new Date();
-
     //new Date() gets the current date and time stored internally as the number of millisecons since midnight Jan 1, 1970
     var today_array = today.toDateString().split(" ");
 
-    document.getElementById("day").innerHTML = 'Date: ' + today_array[0] +', ' +today_array[1] +" "+ today_array[2] +', ' +today_array[3];
+    document.getElementById("day").innerHTML = 'DATE: ' + today_array[0] +', ' +today_array[1] +" "+ today_array[2] +', ' +today_array[3];
 
-    document.getElementById("clock").innerHTML = today.toLocaleTimeString();
+
+    var hoursToMilliseconds = (60 * 60 * 1000);
+    var offsetEST = 3 * hoursToMilliseconds;
+
+    var pacificST =  today.toLocaleTimeString();
+
+    // var easternST = today.toLocaleTimeString();
+  var easternST = new Date ((today.getTime() + offsetEST )).toTimeString().split(' ')[0];
+
+
+    document.getElementById("clock").innerHTML = 'TIME:'+ "<br />" + 'ADA HQ: ' +  pacificST + "<br />" + 'Back East: ' +  easternST;
+
+    // ALTERNATE WAY OF ASSIGNING NEW TEXT TO #clock
+    // $('#clock').text('TIME:\n ADA HQ: ' + today.toLocaleTimeString());
+
+
+
+
+    var offsetMST = 1;
+    var offsetCST = 2;
+    var offsetEST = 3;
+
+    //  var timer= function(){
+    //    var currentTime = new Date().toTimeString().split(' ')[0];
+    //    var mountainST = new Date ((new Date().getTime() + offsetMST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+    //    var centralST = new Date ((new Date().getTime() + offsetCST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+    //    var easternST = new Date ((new Date().getTime() + offsetEST * hoursToMilliseconds )).toTimeString().split(' ')[0];
+    //
+    //     clockDiv.text(currentTime + ' PST\n' + mountainST + ' MST\n' + centralST + ' CST\n' + easternST +  ' EST\n');
+    //     };
+
+
+
+
+
   };
 
+
   setInterval(timer, 1000);
-
-
-
-
-
-
-
-
 
 });

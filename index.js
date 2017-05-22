eec2c9e65c5f0669aebaf746577643ac532b16e1
Brenna <bcmdarroch@gmail.com>
2017-05-21 20:45:01 -0700

    couldn

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
 
-// Use the setInterval method to utilize the function you've created to update each second.
   getTime();
+  // getTime('#EST');
+  // getTime('#CEST');
+  // getTime('#JST');
 
-  var intervalSecond = window.setInterval(getTime, 1000);
+  // Use the setInterval method to utilize the function you've created to update each second.
+  window.setInterval(getTime, 1000);
 
   function getTime() {
     // Use the Date Library in Javascript to retrieve the current date and time information.
     var timeNow = new Date();
+    // I tried using arguments to get this to work but then the callback in the setInterval stopped working mysteriously :C
+    // if ( id == '#PST' ) {
+    //   var timeNow = new Date();
+    // } else if ( id == '#EST' ) {
+    //   var timeNow = new Date() + 180;
+    // } else if ( id == '#CEST' ) {
+    //   var timeNow = new Date() + 480;
+    // } else if ( id == '#PST' ) {
+    //   var timeNow = new Date() + 960;
+    // }
 
     // Use the different methods that are provided to you for retrieving the individual hour, minute and second information.
     var hours = ('0' + timeNow.getHours()).slice(-2);
@@ -17,7 +30,4 @@ $(document).ready(function() {
 
     $('div').html(timeDisplay);
   }
-
-
-
 });

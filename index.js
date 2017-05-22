032dcc5a6aa4a2aed1379aed6bacf9efa8f29f81
Janice Lichtman <jalichtman@gmail.com>
2017-05-22 08:27:49 -0700

    ready to submit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   var timer = function() {
     var today = new Date();
     //new Date() gets the current date and time stored internally as the number of millisecons since midnight Jan 1, 1970
@@ -8,43 +7,19 @@ $(document).ready(function() {
 
     document.getElementById("day").innerHTML = 'DATE: ' + today_array[0] +', ' +today_array[1] +" "+ today_array[2] +', ' +today_array[3];
 
-
-    var hoursToMilliseconds = (60 * 60 * 1000);
-    var offsetEST = 3 * hoursToMilliseconds;
+    var hoursInMilliseconds = (60 * 60 * 1000);
+    var offsetEST = 3 * hoursInMilliseconds;
+    var offsetSwiss = 9 * hoursInMilliseconds;
 
     var pacificST =  today.toLocaleTimeString();
+    var easternST = new Date ((today.getTime() + offsetEST )).toLocaleTimeString();
+    var switzerland = new Date ((today.getTime() + offsetSwiss )).toLocaleTimeString();
 
-    // var easternST = today.toLocaleTimeString();
-  var easternST = new Date ((today.getTime() + offsetEST )).toTimeString().split(' ')[0];
-
-
-    document.getElementById("clock").innerHTML = 'TIME:'+ "<br />" + 'ADA HQ: ' +  pacificST + "<br />" + 'Back East: ' +  easternST;
+    document.getElementById("clock").innerHTML = 'TIME:'+ "<br />" + 'Ada HQ: ' +  pacificST + "<br />" + 'Dad & Friends, aka EST: ' +  easternST + "<br />" + 'Land of Clocks, aka Switzerland, : ' +switzerland;
 
     // ALTERNATE WAY OF ASSIGNING NEW TEXT TO #clock
     // $('#clock').text('TIME:\n ADA HQ: ' + today.toLocaleTimeString());
-
-
-
-
-    var offsetMST = 1;
-    var offsetCST = 2;
-    var offsetEST = 3;
-
-    //  var timer= function(){
-    //    var currentTime = new Date().toTimeString().split(' ')[0];
-    //    var mountainST = new Date ((new Date().getTime() + offsetMST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-    //    var centralST = new Date ((new Date().getTime() + offsetCST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-    //    var easternST = new Date ((new Date().getTime() + offsetEST * hoursToMilliseconds )).toTimeString().split(' ')[0];
-    //
-    //     clockDiv.text(currentTime + ' PST\n' + mountainST + ' MST\n' + centralST + ' CST\n' + easternST +  ' EST\n');
-    //     };
-
-
-
-
-
   };
-
 
   setInterval(timer, 1000);
 

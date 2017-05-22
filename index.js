80ff8cba6c03baca67f33e55e7f4c6668f2f8305
Emma Claire Philippides <acme9898@gmail.com>
2017-05-21 22:49:38 -0700

    javascript function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+$(document).ready(function(){
+
+  function showTime(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+  }
+
+  var clock = document.getElementById('clock');
+
+  clock.innerText = hours + ":" + minutes + ":" + seconds;
+}
+
+showTime();
+});

696d1d509f59a3aa77cbfeb19ac50499c4dedb02
Spatterjaaay <aja.pursova@gmail.com>
2017-05-21 23:22:15 -0700

    discovered toLocaleString method and tried it out

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
 
-  var addZero = function(digit) {
-    if (digit < 10) {
-      digit = "0" + digit;
-    }
-    return digit;
-  };
+  // var addZero = function(digit) {
+  //   if (digit < 10) {
+  //     digit = "0" + digit;
+  //   }
+  //   return digit;
+  // };
 
   var showTime = function() {
-    var today = new Date(),
-    days = addZero(today.getDate()),
-    months = addZero(today.getMonth()),
-    years = today.getFullYear(),
-    hours = today.getHours(),
-    minutes = addZero(today.getMinutes()),
-    seconds = addZero(today.getSeconds()),
-    ampm = hours > 11 ? ' PM' : ' AM',
-    americanHours;
+    var today = new Date();
+    var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: "2-digit", minute: "2-digit", second: "numeric" };
+    var dateTime = today.toLocaleString('en-US', options);
 
-    if (hours === 0) {
-      americanHours = 12;
-    } else if (hours < 13) {
-      americanHours = hours;
-    } else {
-      americanHours = addZero(hours % 12);
-    }
+    // days = addZero(today.getDate()),
+    // months = addZero(today.getMonth()),
+    // years = today.getFullYear(),
+    // hours = today.getHours(),
+    // minutes = addZero(today.getMinutes()),
+    // seconds = addZero(today.getSeconds()),
+    // ampm = hours > 11 ? ' PM' : ' AM',
+    // americanHours;
+    //
+    // if (hours === 0) {
+    //   americanHours = 12;
+    // } else if (hours < 13) {
+    //   americanHours = hours;
+    // } else {
+    //   americanHours = addZero(hours % 12);
+    // }
+    //
+    // $("#date").html(months + "/" + days + "/" + years);
+    // $("#clock").html(americanHours + ":" + minutes + ":" + seconds + ampm);
 
-    $("#date").html(months + "/" + days + "/" + years);
-    $("#clock").html(americanHours + ":" + minutes + ":" + seconds + ampm);
+    $("#clock").html(dateTime);
+
   };
 
   setInterval(showTime, 1000);

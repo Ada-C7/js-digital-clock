d84e1301ba4845a6d00b89f4d5a951e616efd368
Tamiko Terada <tamikodevo@gmail.com>
2017-05-21 19:52:11 -0700

    show someething in the browser

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+
+var time = Date.now();
+var target = document.getElementById('clock');  // Find the HTML element where the ID is js-lecture-target
+target.innerHTML = '<p>' + time + '</p>'; // Put this HTML inside the div we retrieved above
+
+// $(document).ready(function() {
+//   var time = console.log("test");
+//   var display = $('<p>' + time + '</p>');
+//   $('.display').append(display);
+// };

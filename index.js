675375950619c5f62b871ec38b9c2239b066f639
Anacelia Gillette <acgillette426@gmail.com>
2017-05-22 09:26:04 -0700

    Some refactoring

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-// Your code here
+var addZero = function(num) {
+  if(num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+};
+
+
+var setTime = function() {
+  date = new Date();
+  h = addZero(date.getHours());
+  m = addZero(date.getMinutes());
+  s = addZero(date.getSeconds());
+  time = h + ":" + m + ":" + s;
+  $('#clock').text(time);
+};
+
 
 $(document).ready(function() {
-
-
-
-  var setTime = function() {
-    date = new Date();
-    time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    $('#clock').text(time);
-  };
 
   setInterval(setTime, 1000);
 

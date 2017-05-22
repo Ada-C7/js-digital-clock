5f543bcb438d9b5a1b93cbdf4028f2c2753dcc04
Sofia Kim <sofiamhkim@gmail.com>
2017-05-21 21:49:56 -0700

    starttime function complete

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+$(document).ready(function(){
+
+  var startTime = function(time_difference){
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var date = today.getDate();
+    var h = today.getHours() + time_difference;
+    if (h >= 24) {
+      date = date + 1;
+      h = h - 24;
+    }
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    return (year + "/" + month + "/" + date + " " + h + ":" + m + ":" + s);
+  } ;
+
+  var checkTime = function(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+  };
+
+  // var time = startTime()
+  // $('#clock').append(time);
+  $('#la').append(startTime(0));
+  $('#seattle').append(startTime(3));
+  $('#new_york').append(startTime(5)); //call the function and append
+  $('#chicago').append(startTime(5));
+
+  var liveClock = function(){
+    // var clock = document.getElementById('clock');
+    // var clock = $('#clock'); jquery
+    $('#la').html(startTime(0));
+    $('#seattle').html(startTime(3));
+    $('#new_york').html(startTime(5));
+    $('#chicago').html(startTime(5));
+  };
+  setInterval(liveClock, 1000);
+
+});
+
+
+//
+// var checkTime(function(i) {
+//   if (i < 10) {i = "0" + i};
+//   return i;
+// });

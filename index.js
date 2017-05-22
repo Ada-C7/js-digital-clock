a7b49012f1a4dee968203ef95da23ceed280d685
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-21 21:00:37 -0700

    final version

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 // Your code here
 console.log("hello - you got this");
 $(document).ready(function(){
-  // time is assigned to the function date that will reterive the current date
+
   var intervalID = setInterval( function(){ displayTime() }, 1000 );
 
   function displayTime() {
-    // new function getElementById
-    // var clock = document.getElementById('clock');
-    // new function - setting html to element you got by id above
-    // clock.innerHTML = currentTime;
-
     var currentTime = new Date;
 
-    // $('div#clock').html( currentTime.getHours() + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds() );
-    $('div#clock').html( currentTime.toLocaleDateString() + "<div>" + currentTime.toLocaleTimeString() + '</div>'  );
-    // $('div#clock').html(  );
+    $('div#clock').html(
+      currentTime.toLocaleDateString()
+      + "<div>" + currentTime.toLocaleTimeString() + '</div>' );
   };
-
-  // function clearTime() {
-  //   clearInterval(intervalID);
-  // };
 });

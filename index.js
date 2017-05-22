7f37f07deeb2d5f61abb6da72dab937b011204d0
Allison Northrop <agnorthrop@gmail.com>
2017-05-21 17:33:50 -0700

    got time function working

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 // Your code here
 $(document).ready(function() {
-  var date = $('<h1>' + new Date() + '</h1>');
-  // 	var currentHours = date.getHours();
-  // 	var currentMinutes = date.getMinutes();
-  // 	var currentSeconds = date.getSeconds();
-  //   var num = 9;
-  //   // console.log(date, currentHours, currentMinutes, currentSeconds);
-  //
-  // $('.clock').append(date);
-  // $('h1').append(num);
-  $('#clock').append(date);
 
-  for (var i = 0; i < 5; i++) {
-  // Will execute 5 times
-  var listItem = $('<li>' + i + '</li>');
-  $('.dynamic-list').append(listItem);
-}
+  var myVar = setInterval(function(){ time() }, 1000);
+
+  function time() {
+      var currentDate = new Date();
+      var localTime = currentDate.toLocaleTimeString();
+      document.getElementById("clock").innerHTML = localTime;
+      $('#clock').html(localTime);
+  }
+
 
 
 });

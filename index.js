62c6cb2fe0d1eca0d53b7322c5af3f95424b7a46
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-21 19:08:44 -0700

    clock done with date and day

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+$(document).ready(function(){
+  var clock = document.getElementById('clock')
+  var clockDate = document.getElementById('clockDate')
+
+  var digitalClock = function(){
+    var date = new Date();
+    // var time = date.toLocaleTimeString();
+    // var hours = date.getHours().toString();
+    // var minutes = date.getMinutes().toString();
+    // var seconds = date.getSeconds().toString();
+
+    // var twoDigits = function(numString){
+    //   if (numString < 10){
+    //     numString  = "0" + numString;
+    //   }
+    //   return numString
+    // }
+    // var timeString = twoDigits(hours) + ":" + twoDigits(minutes) + ":" + twoDigits(seconds);
+    // clock.textContent = timeString;
+    clock.textContent = date.toLocaleTimeString()
+    clockDate.textContent = date.toDateString()
+
+    // $("#clock").text(timeString);
+
+  }
+
+  setInterval(digitalClock, 1000)
+});

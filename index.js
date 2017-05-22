e2cf41efb05ee590e70ccf71d2a0563447da14f0
Allison Northrop <agnorthrop@gmail.com>
2017-05-21 17:41:19 -0700

    added comments to explain process

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // Your code here
 $(document).ready(function() {
 
+  //sets the inteval to 1000 milliseconds (1 second).
+  //this allows the time fucntion to be called every second.
   var secondsInterval = setInterval(function(){ time() }, 1000);
 
+
   function time() {
+      //date class gets current date
       var currentDate = new Date();
+      //toLocaleTimeString takes the current date and converts it
       var localTime = currentDate.toLocaleTimeString();
+      //jquery calls the div where the id is 'clock' and inputs the localTime. The tim fucntion is being called every second and flashes on screen. 
       $('#clock').html(localTime);
   }
 

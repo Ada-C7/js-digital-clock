3686cef29b7dd2eb68399e6f36fb48dd35595897
EricaJCase <ericajcase@gmail.com>
2017-05-21 22:14:26 -0700

    still working on remembering to commit in js.  a functional

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+// console.log("TEST, TEST");
+$(document).ready(function() { // anonymous function
+
+  console.log("ANOTHER TEST");
+
+  var getTime = function(){
+    console.log("Hey I'm in a clock");
+    timenow = $('<div id = "clock" >' + new Date() + '</div>');
+    $('#clock').replaceWith(timenow);
+  };
+
+
+  var myTimer = setInterval(getTime, 1000);
+});

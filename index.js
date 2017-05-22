8a35f184b25f060f24aa89d0bfc5427d126ca8f9
Cara Comfort <cara.comfort10@gmail.com>
2017-05-21 21:46:08 -0700

    Made digital clock

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+
+
+$(document).ready(function() {
+
+  // so something show up on the page right away
+  // var date = new Date();
+  // $('#clock').html(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+
+  var getTime = function(date) {
+    var date = new Date();
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+
+    // Change to meridian time
+    var meridian = 'AM';
+    if ( hours >= 12 ) {
+      hours = hours - 12;
+      meridian = 'PM';
+    }
+
+    var time = addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
+    time += ' ' + meridian;
+
+    $('#clock').html(time);
+  };
+
+  var addZero = function(time) {
+    return ( time < 10 ) ? "0" + time : time;
+  };
+
+
+
+
+  setInterval(getTime, 1000)
+
+});

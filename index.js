67f611d7200605bf9cf4dc916e851cb9c5c182f0
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-21 21:43:10 -0700

    refactored to use toLocaleTimeString()

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 $(document).ready(function () {
-  var twoDigitString = function (num) {
-    return (num < 10) ? ("0" + num.toString()) : num.toString();
-  };
-
   var calculateTime = function () {
-    var date = new Date(Date.now());
-    var dateString = twoDigitString(date.getHours()) + ":" + twoDigitString(date.getMinutes()) + ":" + twoDigitString(date.getSeconds());
-
-    $('#clock').html(dateString);
+    $('#clock').html(new Date(Date.now()).toLocaleTimeString());
   };
 
   var interval = window.setInterval(calculateTime, 1000);

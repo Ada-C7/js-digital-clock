c7a4e603f6a016265505e6dda553fccd65cea65b
Tamiko Terada <tamikodevo@gmail.com>
2017-05-23 10:16:53 -0700

    add basic clock functionality

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-// Your code here
 
-var time = Date.now();
-var target = document.getElementById('clock');  // Find the HTML element where the ID is js-lecture-target
-target.innerHTML = '<p>' + time + '</p>'; // Put this HTML inside the div we retrieved above
+//as soon as document is loaded
 
-// $(document).ready(function() {
-//   var time = console.log("test");
-//   var display = $('<p>' + time + '</p>');
-//   $('.display').append(display);
-// };
+var toClockString = function(number) {
+  var str_time = number.toString();
+  if (str_time.length === 1) {
+    return "0" + str_time;
+  } else {
+    return str_time;
+  }
+};
+
+var showTime = function(now) {
+  var hours = toClockString(now.getHours()),
+      minutes = toClockString(now.getMinutes()),
+      seconds = toClockString(now.getSeconds());
+
+  var date = now.toDateString();
+  $("#clock").html('<div>' + hours + ":" + minutes + ":" + seconds + '</div>' + '<p>' + date + '</p>');
+};
+
+$(document).ready(function(){
+  setInterval(function(){
+    var now = new Date();
+    showTime(now);
+  }, 1000);
+});

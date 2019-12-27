fd2912c683710a6f616bb68e8451ca10ae9b3140
Kelsey McAlpine <kmcalpine023@gmail.com>
2019-12-26 21:33:36 -0800

    fix updated time w/ setInterval

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var ampm = date.getHours() < 12 ? 'AM' : 'PM';
-    var time = $("<p>" + hours + ":" + minutes + ":" + seconds + " " + ampm + "</p>");
-    $('#clock').append(time);
-  // var timer = setInterval(updateTime, 1000);
+  var startTime = new Date(Date.now()).toLocaleTimeString();
+  $("#clock").append('<p>' + startTime + '</p>');
+
+  var updateTime = function () {
+    var updatedTime = new Date(Date.now()).toLocaleTimeString();
+    $('p').replaceWith('<p>' + updatedTime + '</p>');
+  };
+
+  var changeTime = setInterval(updateTime, 1000);
 });
